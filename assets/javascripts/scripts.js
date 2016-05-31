b0c093cd7965c66930872e25659f74a80aa9a8fb
Chad Danford <chad.m.danford@gmail.com>
2016-05-31 14:51:26 +0200

    mobile function done

---
+++ assets/javascripts/scripts.js
@@ -0,0 +1,11 @@
+// $( document ).ready(function() {
+//
+//
+//     var talent4 = $(".talent-4");
+//     var row2 = $(".flex-row2");
+//
+//
+//     talent4.css({'flex': '0.4'});
+//     row2.css({'flex': '1.2'});
+//
+// });
