a0d0b31b2672104934a1769756e6744f3bd64e06
Chad Danford <chad.m.danford@gmail.com>
2016-06-06 18:01:06 +0200

    type for each headline section

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,12 +1,16 @@
 $( document ).ready(function() {
 
-  var typed = $('.talent-2');
+  var talent = $('.talent');
 
-  typed.mouseover(function(){
-    $(".element").typed({
+
+  talent.mouseover(function(){
+    $(this).find('.typed').stop().fadeIn('fast').typed({
       strings: ["Hello There Friends! This is a Machine Typing."],
       typeSpeed: 0
     });
+  });
+  talent.mouseout(function(){
+    $(this).find('.typed').stop().fadeOut('slow');
   });
 
 
