30825dd2e6f5f8cfa9b73a7f8673ade2b4634c52
Chad Danford <chad.m.danford@gmail.com>
2016-06-07 15:29:48 +0200

    tying machine with subheadlines

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,16 +1,47 @@
 $( document ).ready(function() {
 
-  var talent = $('.talent');
+  var talent1 = $('.talent-1');
+  var talent2 = $('.talent-2');
+  var talent3 = $('.talent-3');
+  var talent4 = $('.talent-4');
 
 
-  talent.mouseover(function(){
-    $(this).find('.typed').stop().fadeIn('fast').typed({
+  talent1.mouseover(function(){
+    $(this).children('.typed').stop().fadeIn('fast').typed({
       strings: ["Hello There Friends! This is a Machine Typing."],
       typeSpeed: 0
     });
   });
-  talent.mouseout(function(){
-    $(this).find('.typed').stop().fadeOut('slow');
+  talent2.mouseover(function(){
+    $(this).children('.typed').stop().fadeIn('fast').typed({
+      strings: ["talent two to too baby"],
+      typeSpeed: 0
+    });
+  });
+  talent3.mouseover(function(){
+    $(this).children('.typed').stop().fadeIn('fast').typed({
+      strings: ["yes baby yes baby yes yes"],
+      typeSpeed: 0
+    });
+  });
+  talent4.mouseover(function(){
+    $(this).children('.typed').stop().fadeIn('fast').typed({
+      strings: ["boogie boogie boogie boogie"],
+      typeSpeed: 0
+    });
+  });
+
+  talent1.mouseout(function(){
+    $(this).children('.typed').stop().fadeOut('slow');
+  });
+  talent2.mouseout(function(){
+    $(this).children('.typed').stop().fadeOut('slow');
+  });
+  talent3.mouseout(function(){
+    $(this).children('.typed').stop().fadeOut('slow');
+  });
+  talent4.mouseout(function(){
+    $(this).children('.typed').stop().fadeOut('slow');
   });
 
 
