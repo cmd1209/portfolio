8bc5ff41071b61800677310a47cfc960c9f8a62e
Chad Danford <chad.m.danford@gmail.com>
2016-08-16 15:25:34 +0200

    profile plus some owl

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -61,5 +61,21 @@ $( document ).ready(function() {
     $(".site-header").removeClass("outoftheway");
   });
 
+  $("#owl-demo").owlCarousel({
+
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1,
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+  });
+
 
 });
