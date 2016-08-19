f63bf466ca76298c46456ac0162f6dd469de25aa
Chad Danford <chad.m.danford@gmail.com>
2016-08-19 14:49:06 +0200

    images for mobile

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -60,19 +60,15 @@ $( document ).ready(function() {
   });
 
   $("#owl-demo").owlCarousel({
-
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-
-        // "singleItem:true" is a shortcut for:
-        // items : 1,
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
-
+    autoPlay : 3000,
+        stopOnHover : true,
+        navigation:false,
+        pagination:false,
+        paginationSpeed : 1000,
+        goToFirstSpeed : 2000,
+        singleItem : true,
+        autoHeight : true,
+        transitionStyle:"fade"
   });
 
 
